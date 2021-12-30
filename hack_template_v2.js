8c0e64674d8e1258306945474e40c53c42bc6fb3
Daniel Albohn <13983830+d-bohn@users.noreply.github.com>
2021-12-29 22:34:15 -0500

    Update hack_template_v2.js

--- hack_template_v2.js
+++ hack_template_v2.js
@@ -1,11 +1,11 @@
 /** @param {NS} ns **/
 export async function main(ns) {
-    
+
     const args = ns.flags([['help', false]]);
-    
-    var target = args._[0];
-    
-    if(args.help || !target) {
+
+    let host = args._[0];
+
+    if (args.help || !host) {
         ns.tprint("This script will generate money by hacking a target server.");
         ns.tprint(`USAGE: run ${ns.getScriptName()} SERVER_NAME`);
         ns.tprint("Example:");
@@ -13,54 +13,51 @@ export async function main(ns) {
         return;
     }
 
-    if (typeof target === "undefined") {
-        target = "n00dles"
-
-    } else {
-        target = ns.args[0]
-    }
-
     // Defines how much money a server should have before we hack it
     // In this case, it is set to 75% of the server's max money
-    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
+    var moneyThresh = ns.getServerMaxMoney(host) * 0.70;
 
     // Defines the maximum security level the target server can
     // have. If the target's security level is higher than this,
     // we'll weaken it before doing anything else
-    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
+    var securityThresh = ns.getServerMinSecurityLevel(host) + 5;
 
     // If we have the BruteSSH.exe program, use it to open the SSH Port
     // on the target server
-    if (ns.fileExists('BruteSSH.exe')) {
-        ns.brutessh(server);
+    if (ns.fileExists('BruteSSH.exe', "home")) {
+        ns.brutessh(host);
+        ns.tprint("SSH Detected. Attempting to BruteSSH.")
     }
-    if (ns.fileExists('FTPCrack.exe')) {
-        ns.ftpcrack(server);
-    }
-    if (ns.fileExists('relaySMTP.exe')) {
-        ns.relaysmtp(server);
-    }
-    if (ns.fileExists('HTTPWorm.exe')) {
-        ns.httpworm(server);
-    }
-    if (ns.fileExists('SQLInject.exe')) {
-        ns.sqlinject(server);
-    }
+    // if (ns.fileExists('FTPCrack.exe', "home")) {
+    //     ns.ftpcrack(host);
+    // }
+    // if (ns.fileExists('relaySMTP.exe', "home")) {
+    //     ns.relaysmtp(host);
+    // }
+    // if (ns.fileExists('HTTPWorm.exe', "home")) {
+    //     ns.httpworm(host);
+    // }
+    // if (ns.fileExists('SQLInject.exe', "home")) {
+    //     ns.sqlinject(host);
+    // }
 
     // Get root access to target server
-    ns.nuke(target);
+    if (!ns.hasRootAccess(host)) {
+        ns.tprint("Root access denied. Attempting to NUKE.");
+        ns.nuke(host);
+    }
 
     // Infinite loop that continously hacks/grows/weakens the target server
     while (true) {
-        if (ns.getServerSecurityLevel(target) > securityThresh) {
+        if (ns.getServerSecurityLevel(host) > securityThresh) {
             // If the server's security level is above our threshold, weaken it
-            await ns.weaken(target);
-        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            await ns.weaken(host);
+        } else if (ns.getServerMoneyAvailable(host) < moneyThresh) {
             // If the server's money is less than our threshold, grow it
-            await ns.grow(target);
+            await ns.grow(host);
         } else {
             // Otherwise, hack it
-            await ns.hack(target);
+            await ns.hack(host);
         }
     }
 }
